26f710f89f61e85a85b40bec835e8cad1acfc4f6
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2017-12-11 16:45:09 +0800

    u-boot-sunxi: add preconfig files

---
+++ u-boot-sunxi/include/config.h
@@ -0,0 +1,14 @@
+/* Automatically generated - do not edit */
+#define CONFIG_SUN8IW7P1	1
+#define CONFIG_SYS_ARCH  "arm"
+#define CONFIG_SYS_CPU   "armv7"
+#define CONFIG_SYS_BOARD "sun8iw7p1"
+#define CONFIG_SYS_VENDOR "sunxi"
+#define CONFIG_SYS_SOC    "sun8iw7p1"
+#define CONFIG_BOARDDIR board/sunxi/sun8iw7p1
+#include <config_cmd_defaults.h>
+#include <config_defaults.h>
+#include <configs/sun8iw7p1.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
+#include <config_uncmd_spl.h>
---
+++ u-boot-sunxi/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = armv7
+BOARD  = sun8iw7p1
+VENDOR = sunxi
+SOC    = sun8iw7p1
