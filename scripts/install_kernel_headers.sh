1d3aafab25ac4ba710e49d3319fc314a12ae314f
August <mingxin.android@gmail.com>
2018-09-20 18:17:51 +0800

    ignore output dir when build kernel headers

--- scripts/install_kernel_headers.sh
+++ scripts/install_kernel_headers.sh
@@ -45,10 +45,11 @@ make ARCH=$LINUX_ARCH CROSS_COMPILE=$CROSS_COMPILE headers_install INSTALL_HDR_P
 
 tar cfh - include | (cd "$TARGET"; umask 000; tar xsf -)
 tar cfh - scripts | (cd "$TARGET"; umask 000; tar xsf -)
-find . -path './scripts/*'   -prune -o -path './Documentation/*' -prune -o  \
-               -path './debian/*'    -prune -o -type f                      \
-               \( -name Makefile -o  -name 'Kconfig*' \) -print  |          \
-                  cpio -pd --preserve-modification-time "$TARGET";
+find . -path './scripts/*'   -prune -o \
+       -path './Documentation/*' -prune -o  \
+       -path './output/*'    -prune -o -type f  \
+       \( -name Makefile -o  -name 'Kconfig*' \) -print  | \
+       cpio -pd --preserve-modification-time "$TARGET";
 
 # Clean compile host executables and replace some with target arch.
 find "$TARGET/scripts" -type f | while read i; do if file -b $i | egrep -q "^ELF.*executable"; then rm "$i"; fi; done
